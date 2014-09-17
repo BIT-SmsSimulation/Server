8bd75692f64db697cae6e7a6a00e4d5b9c0cafc7
chaosdefinition <chaosdefinition@hotmail.com>
2014-09-17 11:15:05 +0800

    Add license; Fix a little problem in sql file.

--- src/main/resource/db_sms.sql
+++ src/main/resource/db_sms.sql
@@ -1,3 +1,24 @@
+/**
+ * SQL DDL file for smsSimulation server.
+ * 
+ * Copyright (c) 2014 BIT-SmsSimulation.
+ * 
+ * This file is part of BIT-SmsSimulation.
+ * 
+ * BIT-SmsSimulation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BIT-SmsSimulation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BIT-SmsSimulation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 DROP DATABASE IF EXISTS `db_sms`;
 CREATE DATABASE IF NOT EXISTS `db_sms`;
 USE `db_sms`;
@@ -21,7 +42,7 @@ CREATE TABLE `message` (
   `CONTENT` text NOT NULL,
   `SENDER_NUM` char(11) NOT NULL,
   `RECEIVER_NUM` char(11) NOT NULL,
-  `time` timestamp NULL DEFAULT NULL,
+  `TIME` timestamp NULL DEFAULT NULL,
   `STATUS` int NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
